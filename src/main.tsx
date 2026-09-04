4a0ae02a1715cad2b5670c29ae6ed7d99f52f46b
Kleber Holtz <kleber.holtz@gmail.com>
2026-09-04 15:23:40 -0300

    feat: initialize internationalization and theme settings in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initI18n } from "./lib/i18n";
+import { initTheme } from "./lib/theme";
+
+initI18n();
+initTheme();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
